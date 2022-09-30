10705829c542aa1a4768adb8621cbc7c14d8f412
Pekstaar <pekstaar@gmail.com>
2022-09-30 20:10:38 +0300

    [UPDATE]: permission role inputs and crud checkboxs'

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,23 +1,33 @@
 import { Button } from "antd";
 import React from "react";
 
-const HeaderBar = ({ handlePress, text, subtext, title, hideBtn = false }) => {
+const HeaderBar = ({
+  handlePress,
+  text,
+  subtext,
+  title,
+  hideBtn = false,
+  children,
+}) => {
   return (
-    <div className="p-4 bg-slate-50 flex justify-between mb-2">
-      <div>
-        <span className="text-xl font-medium text-indigo-800">{title}</span>
-        <br />
-        <span className="text-sm text-indigo-800">{subtext}</span>
-      </div>
+    <div className="bg-slate-50 p-4 flex flex-col mb-4">
+      <div className="  flex justify-between mb-2">
+        <div>
+          <span className="text-xl font-medium text-indigo-800">{title}</span>
+          <br />
+          <span className="text-sm text-indigo-800">{subtext}</span>
+        </div>
 
-      {!hideBtn && (
-        <Button
-          className="bg-indigo-800 font-medium text-gray-100 h-11 px-4 text-md rounded"
-          onClick={handlePress}
-        >
-          {text}
-        </Button>
-      )}
+        {!hideBtn && (
+          <Button
+            className="bg-indigo-800 font-medium text-gray-100 h-11 px-4 text-md rounded"
+            onClick={handlePress}
+          >
+            {text}
+          </Button>
+        )}
+      </div>
+      {children}
     </div>
   );
 };
--- src/pages/permissions/Permissions.js
+++ src/pages/permissions/Permissions.js
@@ -1,4 +1,4 @@
-import { Checkbox, Form, Input } from "antd";
+import { Checkbox, Form, Input, Select } from "antd";
 import React, { useState } from "react";
 import { CustomTable } from "../../components/CustomTable";
 import HeaderBar from "../../components/HeaderBar";
@@ -7,6 +7,7 @@ import CustomModal from "../../components/Modal";
 const Permissions = () => {
   const [openModal, setOpenModal] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const [selectedRole, setSelectedRole] = useState("--select role--");
 
   const handleOpenModal = () => {
     setOpenModal(true);
@@ -26,6 +27,26 @@ const Permissions = () => {
   const handleCheck = (e, data) => {
     // handle data
     console.log(data?.name, "IS CHECKED: ", e.target.checked);
+  };
+  const handleCheckAdd = (e, data) => {
+    // handle data
+    console.log(data?.name, "ADD IS CHECKED: ", e.target.checked);
+  };
+  const handleCheckEdit = (e, data) => {
+    // handle data
+    console.log(data?.name, "EDIT IS CHECKED: ", e.target.checked);
+  };
+  const handleCheckView = (e, data) => {
+    // handle data
+    console.log(data?.name, "VIEW IS CHECKED: ", e.target.checked);
+  };
+  const handleCheckDelete = (e, data) => {
+    // handle data
+    console.log(data?.name, "DELETE IS CHECKED: ", e.target.checked);
+  };
+
+  const handleSelectedRoleChange = (value) => {
+    setSelectedRole(value);
   };
 
   const columns = [
@@ -50,48 +71,101 @@ const Permissions = () => {
       //   sorter: (a, b) => a?.first.localeCompare(b?.first),
     },
 
-    // {
-    //   title: "Actions",
-    //   dataIndex: "actions",
-    //   key: "actions",
-    //   render: (_, data) => (
-    //     <>
-    //       <Button
-    //         className="bg-blue-600 font-medium text-gray-100"
-    //         onClick={() => handleEdit(data)}
-    //       >
-    //         Edit
-    //       </Button>
-    //       &nbsp;
-    //       <Popconfirm
-    //         title="Are you sure to delete this Permission?"
-    //         onConfirm={() => handleDelete(data)}
-    //         okText="Yes"
-    //         cancelText="No"
-    //       >
-    //         <Button className="bg-red-600 font-medium text-gray-100">
-    //           Delete
-    //         </Button>
-    //       </Popconfirm>
-    //     </>
-    //   ),
-    // },
+    // Add checkbox
+    {
+      title: "Add",
+      dataIndex: "add",
+      key: "add",
+      //   sorter: (a, b) => a?.first.localeCompare(b?.first),
+      render: (_, data) => (
+        <>
+          <Checkbox
+            defaultChecked={data?.add}
+            onChange={(e) => handleCheckAdd(e, data)}
+          />
+        </>
+      ),
+    },
+
+    // View Checkbox
+    {
+      title: "View",
+      dataIndex: "view",
+      key: "view",
+      //   sorter: (a, b) => a?.first.localeCompare(b?.first),
+      render: (_, data) => (
+        <>
+          <Checkbox
+            defaultChecked={data?.view}
+            onChange={(e) => handleCheckView(e, data)}
+          />
+        </>
+      ),
+    },
+    // Edit Checkbox
+    {
+      title: "Edit",
+      dataIndex: "edit",
+      key: "edit",
+      //   sorter: (a, b) => a?.first.localeCompare(b?.first),
+      render: (_, data) => (
+        <>
+          <Checkbox
+            defaultChecked={data?.edit}
+            onChange={(e) => handleCheckEdit(e, data)}
+          />
+        </>
+      ),
+    },
+    // Delete checkbox
+    {
+      title: "Delete",
+      dataIndex: "delete",
+      key: "delete",
+      //   sorter: (a, b) => a?.first.localeCompare(b?.first),
+      render: (_, data) => (
+        <>
+          <Checkbox
+            defaultChecked={data?.delete}
+            onChange={(e) => handleCheckDelete(e, data)}
+          />
+        </>
+      ),
+    },
   ];
   return (
     <>
-      <div className="container mx-auto flex flex-col p-3">
+      <div className="container mx-auto flex flex-col p-3 ">
         <HeaderBar
           handlePress={handleOpenModal}
           text={"ADD PERMISSION"}
           title={"Permissions"}
           subtext={"Manage permissions"}
-        />
+        >
+          <div className="w-1/2 mx-auto">
+            <Form.Item required label="Select Role" name="role">
+              <Select
+                defaultValue={selectedRole}
+                onChange={handleSelectedRoleChange}
+              >
+                <Select.Option value="admin">Admin</Select.Option>
+                <Select.Option value="two">User two</Select.Option>
+                <Select.Option value="three">User three</Select.Option>
+                <Select.Option value="four">User four</Select.Option>
+                <Select.Option value="five">User five</Select.Option>
+                <Select.Option value="six">User Six</Select.Option>
+              </Select>
+            </Form.Item>
+          </div>
+        </HeaderBar>
 
-        <CustomTable
-          cols={columns}
-          rows={sampleUsers}
-          style={{ marginTop: "40px" }}
-        />
+        <div>
+          <CustomTable
+            cols={columns}
+            rows={sampleUsers}
+            // style={{ marginTop: "40px" }}
+          />
+        </div>
       </div>
 
       <CustomModal
@@ -116,21 +190,41 @@ const sampleUsers = [
   {
     name: "Lorem ",
     isChecked: false,
+    delete: false,
+    edit: true,
+    add: true,
+    view: true,
   },
   {
     name: " dolor ",
     isChecked: true,
+    delete: false,
+    edit: false,
+    add: false,
+    view: true,
   },
   {
     name: "amet",
     isChecked: false,
+    delete: false,
+    edit: false,
+    add: true,
+    view: true,
   },
   {
     name: " adipisicing.",
     isChecked: true,
+    delete: false,
+    edit: false,
+    add: false,
+    view: true,
   },
   {
     name: "Repellat",
     isChecked: true,
+    delete: true,
+    edit: true,
+    add: true,
+    view: true,
   },
 ];
